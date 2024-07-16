13294919c10262b37f3e05e1555fbf8914114204
pajaro770 <samuel.kamhaji@mail.huji.ac.il>
2024-07-16 09:44:49 +0300

    first

---
+++ hello.cpp
@@ -0,0 +1,5 @@
+//
+// Created by pajaro770 on 7/16/24.
+//
+
+#include "hello.h"
---
+++ hello.h
@@ -0,0 +1,14 @@
+//
+// Created by pajaro770 on 7/16/24.
+//
+
+#ifndef PAINT_BALL_HELLO_H
+#define PAINT_BALL_HELLO_H
+
+
+class hello {
+
+};
+
+
+#endif //PAINT_BALL_HELLO_H
